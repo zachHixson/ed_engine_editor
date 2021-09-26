8c4f690e9cddf87d146fdff6f1c6bc83242956b1
Zach <zHixson.xbdi@gmail.com>
2021-09-25 21:51:06 -0500

    Changed formatting of node templates to reduce vertical space

--- src/common/data_classes/node_libraries/Events.js
+++ src/common/data_classes/node_libraries/Events.js
@@ -9,90 +9,54 @@ const defaultEvents = [
         id: 'event_update',
         outTriggers: ['e_update_trigger_default'],
         outputs: [
-            {
-                id: 'e_update_out_delta_time',
-                type: SOCKET_TYPE.NUMBER,
-            },
+            {id: 'e_update_out_delta_time', type: SOCKET_TYPE.NUMBER},
         ],
     },
     {
         id: 'event_mouse_button',
         outTriggers: ['e_mouse_button_trigger_down', 'e_mouse_button_trigger_up', 'e_mouse_button_trigger_click'],
         outputs: [
-            {
-                id: 'e_mouse_button_out_which',
-                type: SOCKET_TYPE.STRING,
-            },
-            {
-                id: 'e_mouse_button_out_x',
-                type: SOCKET_TYPE.NUMBER,
-            },
-            {
-                id: 'e_mouse_button_out_y',
-                type: SOCKET_TYPE.NUMBER,
-            },
-            {
-                id: 'e_mouse_button_out_object',
-                type: SOCKET_TYPE.OBJECT,
-            },
+            {id: 'e_mouse_button_out_which', type: SOCKET_TYPE.STRING},
+            {id: 'e_mouse_button_out_x', type: SOCKET_TYPE.NUMBER},
+            {id: 'e_mouse_button_out_y', type: SOCKET_TYPE.NUMBER},
+            {id: 'e_mouse_button_out_object', type: SOCKET_TYPE.OBJECT},
         ]
     },
     {
         id: 'event_mouse_move',
         outTriggers: ['e_mouse_move_trigger_default'],
         outputs: [
-            {
-                id: 'e_mouse_move_out_x',
-                type: SOCKET_TYPE.NUMBER,
-            },
-            {
-                id: 'e_mouse_move_out_y',
-                type: SOCKET_TYPE.NUMBER,
-            },
+            {id: 'e_mouse_move_out_x', type: SOCKET_TYPE.NUMBER},
+            {id: 'e_mouse_move_out_y', type: SOCKET_TYPE.NUMBER},
         ],
     },
     {
         id: 'event_keyboard',
         outTriggers: ['e_keyboard_trigger_down', 'e_keyboard_trigger_up', 'e_keyboard_trigger_tap'],
         outputs: [
-            {
-                id: 'e_keyboard_out_which',
-                type: SOCKET_TYPE.STRING,
-            },
+            {id: 'e_keyboard_out_which', type: SOCKET_TYPE.STRING},
         ],
     },
     {
         id: 'event_collision',
         outTriggers: ['e_collision_trigger_start', 'e_collision_trigger_stop'],
         outputs: [
-            {
-                id: 'e_collision_out_object',
-                type: SOCKET_TYPE.OBJECT,
-            },
+            {id: 'e_collision_out_object', type: SOCKET_TYPE.OBJECT},
         ],
     },
     {
         id: 'event_alarm',
         outTriggers: ['e_alarm_default'],
         outputs: [
-            {
-                id: 'e_alarm_out_name',
-                type: SOCKET_TYPE.STRING,
-            },
+            {id: 'e_alarm_out_name', type: SOCKET_TYPE.STRING},
         ],
     },
     {
         id: 'event_message',
         outTriggers: ['e_message_default'],
         outputs: [
-            {
-                id: 'e_message_out_name',
-                type: SOCKET_TYPE.STRING,
-            },
-            {
-                id: 'e_message_out_data',
-                type: SOCKET_TYPE.ANY,
-            },
+            {id: 'e_message_out_name', type: SOCKET_TYPE.STRING},
+            {id: 'e_message_out_data', type: SOCKET_TYPE.ANY},
         ],
     },
 ];
