ab1c40f43b64011919b8ef5d33729ebfe5d1a8ee
Zach <zHixson.xbdi@gmail.com>
2021-11-10 18:29:43 -0600

    Connections can now be removed by draggin on the ends, as well as reconnected

--- src/components/editor_logic/Connection.vue
+++ src/components/editor_logic/Connection.vue
@@ -1,13 +1,38 @@
 <template>
     <div class="connection">
         <svg :width="width" :height="height" ref="svg">
+            <filter id="blur" x="-20%" y="-20%" width="150%" height="150%">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="1.5" />
+            </filter>
+
+            <!--blurred line-->
             <path
-                ref="line"
-                :width="width"
-                :height="height"
+                v-if="mouseOver && isFullConnection"
                 :d="path"
-                stroke="black"
-                fill="none"/>
+                stroke="gray"
+                stroke-width="2"
+                fill="none"
+                filter="url(#blur)"/>
+
+            <!--main line-->
+            <path
+                :d="path"
+                stroke="gray"
+                stroke-width="2"
+                fill="none"
+                :style="mouseOver && isFullConnection ? 'stroke: #444444' : ''"/>
+
+            <!--invisible line that's thicker to use for hover detection-->
+            <path
+                v-if="isFullConnection"
+                class="line"
+                :d="path"
+                stroke="none"
+                stroke-width="8"
+                fill="none"
+                @mouseenter="mouseOver = true"
+                @mouseleave="mouseOver = false"
+                @mousedown="mouseDown"/>
         </svg>
     </div>
 </template>
@@ -16,8 +41,9 @@
 import Victor from 'victor';
 import {SOCKET_TYPE} from '@/common/data_classes/Node_Enums';
 
-const PADDING = 5;
+const PADDING = 20;
 const HANDLE_WIDTH = 100;
+const HANDLE_DIST = 70;
 
 export default {
     name: 'connection',
@@ -31,26 +57,34 @@ export default {
             cssOrigin: new Victor(0, 0),
             path: "",
             mouseMoveHandler: null,
+            mouseDragHandler: null,
+            grabbedHandle: null,
+            navMouseDown: new Victor(0, 0),
             mousePos: new Victor(0, 0),
+            mouseOver: false,
         }
     },
     computed: {
         flipVertical(){
             return (this.startPoint.y > this.endPoint.y);
         },
+        isPartialConnection(){
+            let connection = this.connectionObj;
+            return (!!connection.startSocketEl ^ !!connection.endSocketEl)
+        },
+        isFullConnection(){
+            let connection = this.connectionObj;
+            return (!!connection.startSocketEl && !!connection.endSocketEl);
+        },
     },
     mounted(){
         this.mouseMoveHandler = this.mouseMove.bind(this);
+        this.mouseDragHandler = this.mouseDrag.bind(this);
         this.connectionObj.connectionComponent = this;
 
         //Check if the connection is still in the process of being connected
-        if (!!this.connectionObj.startSocketEl ^ !!this.connectionObj.endSocketEl){
-            window.addEventListener('mousemove', this.mouseMoveHandler);
-            window.addEventListener('mouseup', ()=>{
-                window.removeEventListener('mousemove', this.mouseMoveHandler);
-                this.connectSocket();
-            }, {once: true});
-
+        if (this.isPartialConnection){
+            this.registerConnectEvents();
             this.$nextTick(()=>{
                 this.setInitialMousePos();
                 this.update();
@@ -59,17 +93,26 @@ export default {
     },
     beforeDestroy(){
         window.removeEventListener('mousemove', this.mouseMoveHandler);
+        window.removeEventListener('mousemove', this.mouseDragHandler);
         this.connectionObj.startSocketEl = null;
         this.connectionObj.endSocketEl = null;
         this.connectionObj.registerUpdateCallback = null;
     },
     methods: {
+        registerConnectEvents(){
+            window.addEventListener('mousemove', this.mouseMoveHandler);
+            window.addEventListener('mouseup', ()=>{
+                window.removeEventListener('mousemove', this.mouseMoveHandler);
+                this.connectSocket();
+            }, {once: true});
+        },
         relink(nodeInfoMap){
             let start = nodeInfoMap.get(this.connectionObj.startNode.nodeId);
             let end = nodeInfoMap.get(this.connectionObj.endNode.nodeId);
             let startSocketEl = start.sockets.get(this.connectionObj.startSocketId);
             let endSocketEl = end.sockets.get(this.connectionObj.endSocketId);
 
+            this.connectionObj.canConnect = true;
             this.connectionObj.startSocketEl = startSocketEl.$refs.socketConnection;
             this.connectionObj.endSocketEl = endSocketEl.$refs.socketConnection;
 
@@ -147,10 +190,59 @@ export default {
                            ${endCoord.x - HANDLE_WIDTH - PADDING} ${endCoord.y},
                            ${endCoord.x} ${endCoord.y}`;
         },
+        mouseDown(event){
+            if (event.which != 1){
+                return;
+            }
+
+            let clientMouseDown = new Victor(event.clientX, event.clientY);
+            this.navMouseDown.copy(this.clientToNavSpace(clientMouseDown));
+            let distToStart = this.navMouseDown.distance(this.startPoint);
+            let distToEnd = this.navMouseDown.distance(this.endPoint);
+
+            if (distToEnd < HANDLE_DIST){
+                this.grabbedHandle = 1;
+            }
+            else if (distToStart < HANDLE_DIST){
+                this.grabbedHandle = -1;
+            }
+
+            if (this.grabbedHandle){
+                window.addEventListener('mousemove', this.mouseDragHandler);
+                window.addEventListener('mouseup', ()=>{
+                    window.removeEventListener('mousemove', this.mouseDragHandler);
+                }, {once: true});
+            }
+        },
         mouseMove(event){
-            let clientMousePos = new Victor(event.clientX, event.clientY);
-            this.mousePos.copy(this.clientToNavSpace(clientMousePos));
+            this.updateMousePos(event);
             this.update();
+        },
+        mouseDrag(event){
+            this.updateMousePos(event);
+
+            let dragDistance = this.navMouseDown.distance(this.mousePos);
+
+            if (dragDistance > 5){
+                if (this.grabbedHandle == 1){
+                    this.connectionObj.endSocketId = null;
+                    this.connectionObj.endSocketEl = null;
+                }
+                else{
+                    this.connectionObj.startSocketId = null;
+                    this.connectionObj.startSocketEl = null;
+                }
+
+                this.registerConnectEvents();
+                this.grabbedHandle = null;
+                window.removeEventListener('mousemove', this.mouseDragHandler);
+            }
+        },
+        updateMousePos(event){
+            let clientMousePos = new Victor(event.clientX, event.clientY);
+            let navMousePos = this.clientToNavSpace(clientMousePos);
+
+            this.mousePos.copy(navMousePos);
         },
         centerFromBounds(bounds){
             let ul = new Victor(bounds.left, bounds.top);
@@ -188,6 +280,8 @@ export default {
                 this.connectionObj.startSocketId = this.curSocketOver.socketData.id;
                 this.connectionObj.startSocketEl = this.curSocketOver.socketEl;
             }
+
+            this.mouseOver = false;
 
             this.$nextTick(()=>{
                 this.checkLoop();
@@ -243,5 +337,9 @@ function _checkLoop(connection, connectionMap, checkedNodes){
     position: absolute;
     left: 0;
     top: 0;
+}
+
+.line{
+    pointer-events: visible;
 }
 </style>
