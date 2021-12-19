fd6ec535345d5e82dade510d5a477c6ecb199091
Zach <zHixson.xbdi@gmail.com>
2021-12-18 23:02:40 -0600

    Removed redundant clamp method

--- src/common/Util_2D.js
+++ src/common/Util_2D.js
@@ -11,10 +11,6 @@ export function isInBounds(x, y, lowX, lowY, highX, highY){
     );
 }
 
-export function clamp(val, min, max){
-    return Math.min(Math.max(val, min), max);
-}
-
 export function getSpriteDimensions(spriteArr){
     return Math.round(Math.sqrt(spriteArr.length));
 }
