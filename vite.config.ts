6820e9b0db4b13fb271e4e12ed53290bd38f1974
Zach <zHixson.xbdi@gmail.com>
2023-01-02 15:38:52 -0600

    Modified vite.config to allow fo conditional build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,62 @@
 import { fileURLToPath, URL } from 'node:url'
 import filterReplace from 'vite-plugin-filter-replace';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({}),
-    filterReplace([
-      {
-        filter: /core\.ts$/g,
-        replace: {
-          from: "export { Engine } from '@engine/Engine'",
-          to: "export { Engine } from '@compiled/Engine'"
-        }
-      },
-      {
-        filter: /core\.ts$/g,
-        replace: {
-          from: "import * as Core from '@engine/core/core';",
-          to: "import {Core} from '@compiled/Engine'",
+export default defineConfig(({mode}) => {
+    const filterReplaceArgs = [
+        {
+            filter: /core\.ts$/g,
+            replace: {
+                from: "export { Engine } from '@engine/Engine'",
+                to: "export { Engine } from '@compiled/Engine'"
+            }
         },
-      },
-    ],
-    {
-      enforce: 'pre',
-      apply: 'build',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@compiled': fileURLToPath(new URL('./_compiled', import.meta.url)),
-      '@engine': fileURLToPath(new URL('./engine', import.meta.url)),
+        {
+            filter: /core\.ts$/g,
+            replace: {
+                from: "import * as Core from '@engine/core/core';",
+                to: "import {Core} from '@compiled/Engine'",
+            },
+        },
+    ];
+    const plugins = [
+        vue(),
+        VueI18nPlugin({}),
+        filterReplace(filterReplaceArgs, {
+            enforce: 'pre',
+            apply: 'build',
+        }),
+        filterReplace([
+            {
+                filter: /\.html$/g,
+                replace: {
+                    from: '/assets/',
+                    to: './assets/'
+                }
+            }
+        ],{
+            enforce: 'post',
+            apply: 'build',
+        }),
+    ];
+
+    if (mode == 'portable'){
+        plugins.push(viteSingleFile());
     }
-  }
+
+    return {
+        plugins,
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@compiled': fileURLToPath(new URL('./_compiled', import.meta.url)),
+                '@engine': fileURLToPath(new URL('./engine', import.meta.url)),
+            }
+        },
+    }
 })
