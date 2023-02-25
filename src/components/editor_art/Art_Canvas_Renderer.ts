fd31dc0f7229bebbdfa6b87daef6fd9948ea6da4
Zach <zHixson.xbdi@gmail.com>
2023-02-25 00:07:36 -0600

    Initial work on converting Art Editor to webGL. Checker BG and grid drawing now work.

--- src/components/editor_art/Art_Canvas_Renderer.ts
+++ src/components/editor_art/Art_Canvas_Renderer.ts
@@ -1,185 +1,161 @@
 import Core from '@/core';
 
+const { WGL } = Core;
+
+let test = 0;
+
 export default class Art_Canvas_Renderer{
+    private static readonly _vertexSource = `
+        uniform vec2 u_dimensions;
+        uniform mat3 u_viewMatrix;
+
+        attribute vec4 a_position;
+        
+        varying vec2 vUv;
+        varying vec2 vScreenCoord;
+        
+        void main(){
+            vec2 aspect = vec2(u_dimensions.y / u_dimensions.x, 1.0);
+            vec4 aspPos = vec4(a_position.xy * aspect, 1.0, 1.0);
+
+            vUv = (a_position.xy + 1.0) / 2.0;
+            gl_Position = vec4(aspPos.xyz * u_viewMatrix, 1.0);
+
+            vScreenCoord = gl_Position.xy * aspect;
+        }
+    `;
+    private static readonly _fragmentSource = `
+        precision highp float;
+
+        const float GRID_SCALE = 30.0;
+
+        varying vec2 vUv;
+        varying vec2 vScreenCoord;
+
+        void main(){
+            //checker bg
+            float x = vScreenCoord.x;
+            x = floor(mod(x * GRID_SCALE, 2.0));
+            float y = vScreenCoord.y + (x * (1.0 / GRID_SCALE));
+            y = floor(mod(y * GRID_SCALE, 2.0));
+            float bg = mix(0.75, 0.8, y);
+
+            //grid
+            vec2 gUv = abs(fract(vUv * 16.0) - 0.5) * 2.0;
+            float grid = max(gUv.x, gUv.y);
+            grid = smoothstep(1.0, 0.90, grid);
+
+            //composite
+            vec3 col = vec3(mix(grid, bg, grid));
+            
+            gl_FragColor = vec4(col, 1.0);
+        }
+    `;
+    private static readonly _planeGeo = [
+        -1, 1,
+        1, 1,
+        1, -1,
+        1, -1,
+        -1, -1,
+        -1, 1
+    ];
+
     private readonly GRID_DIV = Core.Sprite.DIMENSIONS;
     private readonly CANVAS_WIDTH = this.GRID_DIV * 20;
 
-    private canvas: HTMLCanvasElement;
-    private checkerBGBuff: HTMLCanvasElement;
-    private checkerStencilBuff: HTMLCanvasElement;
-    private pixelBuff: HTMLCanvasElement;
-    private gridBuff: HTMLCanvasElement;
-    private previewBuff: HTMLCanvasElement;
-    private spriteData: ImageData;
-    private previewData: ImageData;
-    private navState: Core.iNavState;
+    private _nextDrawCall: number | null = null;
+    private _navState: Core.iNavState;
+    private _canvas: HTMLCanvasElement;
+    private _gl: WebGL2RenderingContext;
+    private _program: WebGLProgram;
+    private _dimensionUniformLoc: WebGLUniformLocation;
+    private _viewMatrixUniformLoc: WebGLUniformLocation;
+    private _positionAttribLoc: number;
+    private _positionBuffer: WebGLBuffer;
+    private _vao: WebGLVertexArrayObject;
 
     constructor(element: HTMLCanvasElement, spriteData: ImageData, previewData: ImageData, navState: Core.iNavState){
-        this.canvas = element;
-        this.checkerBGBuff = Core.Draw.createHDPICanvas(this.canvas.width, this.canvas.height);
-        this.checkerStencilBuff = Core.Draw.createHDPICanvas(this.canvas.width, this.canvas.height);
-        this.pixelBuff = Core.Draw.createCanvas(this.GRID_DIV, this.GRID_DIV);
-        this.gridBuff = Core.Draw.createHDPICanvas(this.canvas.width, this.canvas.height);
-        this.previewBuff = Core.Draw.createCanvas(this.GRID_DIV, this.GRID_DIV);
-        this.spriteData = spriteData;
-        this.previewData = previewData;
-        this.navState = navState;
+        this._navState = navState;
+        this._canvas = element;
 
-        this.fullRedraw();
+        //Setup webGL context
+        this._gl = WGL.getGLContext(this._canvas)!;
+        this._program = WGL.createProgram(
+            this._gl,
+            WGL.createShader(this._gl, this._gl.VERTEX_SHADER, Art_Canvas_Renderer._vertexSource)!,
+            WGL.createShader(this._gl, this._gl.FRAGMENT_SHADER, Art_Canvas_Renderer._fragmentSource)!
+        )!;
+
+        this._dimensionUniformLoc = this._gl.getUniformLocation(this._program, 'u_dimensions')!;
+        this._viewMatrixUniformLoc = this._gl.getUniformLocation(this._program, 'u_viewMatrix')!;
+        this._positionAttribLoc = this._gl.getAttribLocation(this._program, 'a_position');
+
+        this._positionBuffer = this._gl.createBuffer()!;
+        this._vao = this._gl.createVertexArray()!;
+
+        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._positionBuffer);
+        this._gl.bufferData(this._gl.ARRAY_BUFFER, new Float32Array(Art_Canvas_Renderer._planeGeo), this._gl.STATIC_DRAW);
+        this._gl.bindVertexArray(this._vao);
+        this._gl.enableVertexAttribArray(this._positionAttribLoc);
+        this._gl.vertexAttribPointer(this._positionAttribLoc, 2, this._gl.FLOAT, false, 0, 0);
+        this._gl.clearColor(1, 1, 1, 1);
+        this._gl.useProgram(this._program);
+        this._gl.uniformMatrix3fv(this._viewMatrixUniformLoc, false, [
+            .5, 0, 0,
+            0, .5, 0,
+            0, 0, 1
+        ]);
+
+        this.resize();
+        this.queueRender();
     }
 
-    getTranslate(): Core.Vector {
-        return new Core.Vector(
-            (this.canvas.width / 2) + (this.navState.offset.x * this.navState.zoomFac),
-            (this.canvas.height / 2) + (this.navState.offset.y * this.navState.zoomFac)
-        );
+    queueRender(){
+        if (!this._nextDrawCall){
+            this._nextDrawCall = requestAnimationFrame(()=>{
+                this.render()
+                this._nextDrawCall = null;
+            });
+        }
     }
 
-    fullRedraw(): void {
-        const ctx = this.canvas.getContext("2d")!;
-
-        this.drawCheckerBG();
-        this.drawBGStencil();
-        this.drawSpriteData();
-        this.drawGrid();
-        this.drawPreviewBuffer();
-
-        this.composite(ctx);
+    render(): void {
+        this._gl.clear(this._gl.COLOR_BUFFER_BIT);
+        this._gl.useProgram(this._program);
+        this._gl.bindVertexArray(this._vao);
+        this._gl.uniform2f(this._dimensionUniformLoc, this._gl.canvas.width, this._gl.canvas.height);
+        this._gl.uniformMatrix3fv(this._viewMatrixUniformLoc, false, [
+            .5, 0, Math.sin((test++) * 0.05) * 0.5,
+            0, .5, 0,
+            0, 0, 1
+        ]);
+        this._gl.drawArrays(this._gl.TRIANGLES, 0, 6);
     }
 
-    composite(ctx = this.canvas.getContext("2d")!): void {
-        const HALF_CANVAS = this.CANVAS_WIDTH / 2;
-
-        const scaleFac = (this.CANVAS_WIDTH / this.GRID_DIV) * this.navState.zoomFac;
-        const halfScale = -HALF_CANVAS * this.navState.zoomFac;
-        const translate = this.getTranslate();
-
-        ctx.drawImage(this.checkerBGBuff, 0, 0, this.checkerBGBuff.width, this.checkerBGBuff.height);
-
-        //draw pixel and preview buffers (they require a transformation)
-        ctx.imageSmoothingEnabled = false;
-
-        ctx.translate(translate.x, translate.y);
-        ctx.translate(halfScale, halfScale)
-        ctx.scale(scaleFac, scaleFac);
-
-        ctx.drawImage(this.pixelBuff, 0, 0, this.pixelBuff.width, this.pixelBuff.height);
-        ctx.drawImage(this.previewBuff, 0, 0, this.previewBuff.width, this.previewBuff.height);
-
-        ctx.resetTransform();
-
-        //draw buffers that don't require a transformation
-        ctx.drawImage(this.gridBuff, 0, 0, this.gridBuff.width, this.gridBuff.height);
-        ctx.drawImage(this.checkerStencilBuff, 0, 0, this.checkerStencilBuff.width, this.checkerStencilBuff.height);
+    resize(): void {
+        this._gl.viewport(0, 0, this._canvas.width, this._canvas.height);
     }
 
-    resize(width = this.canvas.clientWidth, height = this.canvas.clientHeight): void {
-        Core.Draw.resizeHDPICanvas(this.checkerBGBuff, width, height);
-        Core.Draw.resizeHDPICanvas(this.checkerStencilBuff, width, height);
-        Core.Draw.resizeHDPICanvas(this.gridBuff, width, height);
-
-        this.fullRedraw();
-    }
-
-    navChanged(): void {        
-        this.drawBGStencil();
-        this.drawSpriteData();
-        this.drawGrid();
-        this.drawPreviewBuffer();
-        
-        this.composite();
+    navChanged(): void {
+        //this._gl.uniformMatrix3fv(this._viewMatrixUniformLoc, false, this._navState.matrix.data);
+        //this.queueRender();
     }
 
     setSprite(newSprite: ImageData, navRef: Core.iNavState): void {
-        this.spriteData = newSprite;
-        this.navState = navRef;
+        this._navState = navRef;
 
-        this.fullRedraw();
+        this.queueRender();
     }
 
     mouseDown(): void {
-        this.drawSpriteData();
-        this.drawPreviewBuffer();
-        this.composite();
+        this.queueRender();
     }
 
     mouseUp(): void {
-        this.drawSpriteData();
-        this.drawPreviewBuffer();
-        this.composite();
+        this.queueRender();
     }
 
     mouseMove(): void {
-        this.drawSpriteData();
-        this.drawPreviewBuffer();
-        this.composite();
-    }
-
-    drawCheckerBG(canvas = this.checkerBGBuff): void {
-        Core.Draw.drawCheckerBG(canvas, 10, "#B5B5B5", "#CCCCCC");
-    }
-
-    drawBGStencil(ctx = this.checkerStencilBuff.getContext('2d')!): void {
-        const FULL_WIDTH = this.CANVAS_WIDTH * this.navState.zoomFac;
-        const HALF_WIDTH = -FULL_WIDTH / 2;
-        const canvas = ctx.canvas;
-
-        const translate = this.getTranslate();
-
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, this.checkerStencilBuff.width, this.checkerStencilBuff.height);
-        ctx.translate(translate.x, translate.y);
-        ctx.clearRect(HALF_WIDTH, HALF_WIDTH, FULL_WIDTH, FULL_WIDTH);
-        ctx.resetTransform();
-
-        //remove after debugging
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, 10, 10);
-        ctx.fillRect(canvas.width - 10, canvas.height - 10, 10, 10);
-    }
-
-    drawPixelData(canvas: HTMLCanvasElement, pixelData: ImageData): void {
-        if (pixelData){
-            canvas.getContext('2d')!.putImageData(pixelData, 0, 0);
-        }
-    }
-
-    drawSpriteData(canvas = this.pixelBuff): void {
-        this.drawPixelData(canvas, this.spriteData);
-    }
- 
-    drawGrid(ctx = this.gridBuff.getContext("2d")!): void {
-        const PIXEL_SIZE = Math.round(this.CANVAS_WIDTH / this.GRID_DIV);
-        const HALF_CANVAS = this.CANVAS_WIDTH / 2;
-
-        let translate = this.getTranslate();
-
-        ctx.clearRect(0, 0, this.gridBuff.width, this.gridBuff.height);
-
-        ctx.translate(translate.x, translate.y);
-        ctx.scale(this.navState.zoomFac, this.navState.zoomFac);
-
-        //draw grid
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-        for (let i = 1; i < this.GRID_DIV; i++) {
-            let curLine = i * PIXEL_SIZE;
-            let pos = new Core.Vector(curLine, curLine);
-
-            pos.subtractScalar(HALF_CANVAS);
-
-            ctx.moveTo(pos.x, -HALF_CANVAS);
-            ctx.lineTo(pos.x, HALF_CANVAS);
-            ctx.moveTo(-HALF_CANVAS, pos.y);
-            ctx.lineTo(HALF_CANVAS, pos.y);
-        }
-
-        ctx.resetTransform();
-        ctx.stroke();
-    }
-
-    drawPreviewBuffer(canvas = this.previewBuff): void {
-        this.drawPixelData(canvas, this.previewData);
+        this.queueRender();
     }
 }
