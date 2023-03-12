103ed57a6d0366e477079ff3dd649dc9eddfc1f9
Zach <zHixson.xbdi@gmail.com>
2023-03-11 20:59:34 -0600

    Restructured Instance_Renderer to be a component of Room_Edit_Renderer instead of an abstract base class. This will allow multiple Instance_Renderers to be "layered," together.

--- engine/core/wgl/Instance_Renderer.ts
+++ engine/core/wgl/Instance_Renderer.ts
@@ -1,12 +1,10 @@
 import { WGL } from "../core";
-import { NavState } from "../NavState";
 import { Mat3 } from "../Mat3";
-import { Vector } from "../Vector";
 
-export abstract class Instance_Renderer {
-    private static _planeGeo = WGL.createPlaneGeo().map(i => (i + 1) * 8);
-    private static _planeUVs = WGL.createPlaneGeo().map(i => (i + 1) * 0.5);
-    private static _vertexSource = `
+export class Instance_Renderer {
+    private static readonly _planeGeo = WGL.createPlaneGeo().map(i => (i + 1) * 8);
+    private static readonly _planeUVs = WGL.createPlaneGeo().map(i => (i + 1) * 0.5);
+    private static readonly _vertexSource = `
         uniform mat3 u_viewMatrix;
         uniform vec2 u_dimensions;
 
@@ -24,7 +22,7 @@ export abstract class Instance_Renderer {
             gl_Position = vec4(clipPos, 1.0);
         }
     `;
-    private static _fragmentSource = `
+    private static readonly _fragmentSource = `
         precision highp float;
 
         varying vec2 v_uv;
@@ -34,25 +32,18 @@ export abstract class Instance_Renderer {
         }
     `;
 
-    protected _canvas: HTMLCanvasElement;
-    protected _navState: NavState;
-    protected _gl: WebGL2RenderingContext;
-    protected _instanceProgram: WebGLProgram;
-    protected _viewMatrixUniform: WGL.Uniform_Object;
-    protected _dimensionUniform: WGL.Uniform_Object;
-    protected _planeVertsAttribute: WGL.Attribute_Object;
-    protected _positionAttribute: WGL.Attribute_Object;
-    protected _uvAttribute: WGL.Attribute_Object;
-    protected _instanceVAO: WebGLVertexArrayObject;
-    protected _viewMatrix = new Mat3();
-    protected _viewMatrixNeedsUpdate = true;
+    private _gl: WebGL2RenderingContext;
+    private _instanceProgram: WebGLProgram;
+    private _viewMatrixUniform: WGL.Uniform_Object;
+    private _dimensionUniform: WGL.Uniform_Object;
+    private _planeVertsAttribute: WGL.Attribute_Object;
+    private _positionAttribute: WGL.Attribute_Object;
+    private _uvAttribute: WGL.Attribute_Object;
+    private _instanceVAO: WebGLVertexArrayObject;
 
-    constructor(canvas: HTMLCanvasElement, navState: NavState){
-        this._canvas = canvas;
-        this._navState = navState;
-
+    constructor(gl: WebGL2RenderingContext){
         //setup webGL context
-        this._gl = WGL.getGLContext(this._canvas)!;
+        this._gl = gl;
         this._instanceProgram = WGL.createProgram(
             this._gl,
             WGL.createShader(this._gl, this._gl.VERTEX_SHADER, Instance_Renderer._vertexSource)!,
@@ -76,41 +67,21 @@ export abstract class Instance_Renderer {
         ]), 2, this._gl.FLOAT, false, 0, 0, this._gl.DYNAMIC_DRAW);
         this._positionAttribute.setDivisor(1);
         this._uvAttribute.set(new Float32Array(Instance_Renderer._planeUVs), 2);
-
-        this._gl.clearColor(0, 0, 0, 1);
-
-        this.render();
-    }
-
-    protected _updateViewMatrix(): void {
-        if (!this._viewMatrixNeedsUpdate) return;
-
-        const zoom = this._navState.zoomFac;
-        const { x, y } = this._navState.offset;
-        const dimensions = new Vector(this._canvas.width / 2, this._canvas.height / 2);
-        const zoomMat = new Mat3([zoom, 0, 0, 0, zoom, 0, 0, 0, 1]);
-        const tranMat = new Mat3([1, 0, x, 0, 1, y, 0, 0, 1]);
-        const aspectMat = new Mat3([
-            1.0 / dimensions.x, 0, 0,
-            0, 1.0 / dimensions.y, 0,
-            0, 0, 1.0
-        ]);
-        this._viewMatrix.copy(zoomMat.multiply(aspectMat).multiply(tranMat));
-        this._viewMatrixUniform.set(false, this._viewMatrix.data);
-        this._viewMatrixNeedsUpdate = false;
     }
 
     resize(): void {
-        this._gl.viewport(0, 0, this._canvas.width, this._canvas.height);
+        this._gl.useProgram(this._instanceProgram);
         this._dimensionUniform.set(this._gl.canvas.width / 2, this._gl.canvas.height / 2);
-        this._viewMatrixNeedsUpdate = true;
+    }
+
+    updateViewMatrix(viewMat: Mat3): void {
+        this._gl.useProgram(this._instanceProgram);
+        this._viewMatrixUniform.set(false, viewMat.data);
     }
 
     render(): void {
         this._gl.bindVertexArray(this._instanceVAO);
         this._gl.useProgram(this._instanceProgram);
-        this._gl.clear(this._gl.COLOR_BUFFER_BIT);
-        this._updateViewMatrix();
         this._gl.drawArraysInstanced(
             this._gl.TRIANGLES,
             0,
--- src/components/editor_room/Room_Edit_Renderer.ts
+++ src/components/editor_room/Room_Edit_Renderer.ts
@@ -6,18 +6,49 @@ import objectIconRaw from '@/assets/object_icon.svg?raw';
 import exitIconRaw from '@/assets/exit.svg?raw';
 import endIconRaw from '@/assets/end.svg?raw';
 
-const { Vector, Draw, WGL } = Core;
+const { Vector, Mat3, Draw, WGL } = Core;
 
 const NO_SPRITE_PADDING = 0.85;
 let iconsLoaded = false;
 
-export default class Room_Edit_Renderer extends Core.Instance_Renderer {
+export default class Room_Edit_Renderer {
+    private _nextDrawCall: number | null = null;
+    private _canvas: HTMLCanvasElement;
+    private _navState: Core.NavState;
+    private _gl: WebGL2RenderingContext;
+    private _instanceRenderer: Core.Instance_Renderer;
+    private _viewMatrix = new Mat3();
+    private _viewMatrixNeedsUpdate = true;
+
     constructor(canvas: HTMLCanvasElement, navState: Core.NavState){
-        super(canvas, navState);
+        this._canvas = canvas;
+        this._navState = navState;
+        this._gl = WGL.getGLContext(this._canvas)!;
+        this._instanceRenderer = new Core.Instance_Renderer(this._gl);
+
+        this._gl.clearColor(0, 0, 0, 1);
     }
 
     get CELL_PX_WIDTH(){return 50};
     get UNIT_WIDTH(){return this.CELL_PX_WIDTH / 16};
+
+    private _updateViewMatrix(): void {
+        if (!this._viewMatrixNeedsUpdate) return;
+
+        const zoom = this._navState.zoomFac;
+        const { x, y } = this._navState.offset;
+        const dimensions = new Vector(this._gl.canvas.width / 2, this._gl.canvas.height / 2);
+        const zoomMat = new Mat3([zoom, 0, 0, 0, zoom, 0, 0, 0, 1]);
+        const tranMat = new Mat3([1, 0, x, 0, 1, y, 0, 0, 1]);
+        const aspectMat = new Mat3([
+            1.0 / dimensions.x, 0, 0,
+            0, 1.0 / dimensions.y, 0,
+            0, 0, 1.0
+        ]);
+        this._viewMatrix.copy(zoomMat.multiply(aspectMat).multiply(tranMat));
+        this._instanceRenderer.updateViewMatrix(this._viewMatrix);
+        this._viewMatrixNeedsUpdate = false;
+    }
 
     getMouseCell(){
         return //this.mouseCell;
@@ -45,7 +76,7 @@ export default class Room_Edit_Renderer extends Core.Instance_Renderer {
 
     navChanged(): void {
         this._viewMatrixNeedsUpdate = true;
-        this.render();
+        this.queueRender();
     }
 
     instancesChanged(){
@@ -57,20 +88,24 @@ export default class Room_Edit_Renderer extends Core.Instance_Renderer {
     }
 
     resize(){
-        super.resize();
-        this.render();
+        this._gl.viewport(0, 0, this._gl.canvas.width, this._gl.canvas.height);
+        this._instanceRenderer.resize();
+        this.queueRender();
     }
 
-    fullRedraw(){
-        //
+    queueRender(): void {
+        if (!this._nextDrawCall){
+            this._nextDrawCall = requestAnimationFrame(()=>{
+                this.render();
+                this._nextDrawCall = null;
+            });
+        }
     }
 
-    _composite(){
-        //
-    }
-
-    _drawObjects(){
-        //
+    render(): void {
+        this._updateViewMatrix();
+        this._gl.clear(this._gl.COLOR_BUFFER_BIT);
+        this._instanceRenderer.render();
     }
 
     screenToWorldPos(pt: Core.Vector){
