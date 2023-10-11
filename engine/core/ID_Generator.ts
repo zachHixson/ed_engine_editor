6ab5a923205bcf4e65b108b874722f40af5af7e2
Zach <zHixson.xbdi@gmail.com>
2023-10-11 16:57:52 -0500

    Converted ID_Generator to static class

--- engine/core/ID_Generator.ts
+++ engine/core/ID_Generator.ts
@@ -1,21 +1,21 @@
-export const ID_Generator = {
-    curStep: 0,
+export class ID_Generator {
+    private static _curStep = 0;
 
-    newID(): number {
-        let id = this.curStep;
-        this.curStep++;
+    static newID = (): number => {
+        let id = this._curStep;
+        this._curStep++;
         return id;
-    },
-
-    reset(): void {
-        this.curStep = 0;
-    },
-
-    getCurrentID(): number {
-        return this.curStep;
-    },
-
-    setID(id: number): void {
-        this.curStep = id;
     }
-};
+    
+    static reset = (): void => {
+        this._curStep = 0;
+    }
+    
+    static getCurrentID = (): number => {
+        return this._curStep;
+    }
+    
+    static setID = (id: number): void => {
+        this._curStep = id;
+    }
+}
