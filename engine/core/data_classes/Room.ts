322929ed94ab7350f5a60c34409843a4d92e57ed
Zach <zHixson.xbdi@gmail.com>
2024-01-23 18:31:00 -0600

    Optimized 'clearSpacialCollection' to no longer sort items every time it's cleared

--- engine/core/data_classes/Room.ts
+++ engine/core/data_classes/Room.ts
@@ -148,8 +148,6 @@ export class Room extends Asset_Base {
     }
 
     clearSpacialData(): void {
-        if (!this._spacialCollection) return;
-        this._instances.sort((a, b) => a.zDepth < b.zDepth);
         this._spacialCollection = null;
     }
 
