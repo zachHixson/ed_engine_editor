ff1e691dfb08aa64c1b1effdd72c4dc9ec909c1e
Zach <zHixson.xbdi@gmail.com>
2025-11-15 14:52:40 -0600

    Small formatting tweak to build_engine.js

--- scripts/build_engine.js
+++ scripts/build_engine.js
@@ -6,45 +6,47 @@ if (!fs.existsSync('./_compiled/FontData.js') || process.argv.includes('--build-
     execSync('npm run build-font');
 }
 
-require("esbuild").build({
-    entryPoints: ['..\\engine\\Engine.ts'],
-    outfile: '.\\_compiled\\engine_raw.js',
-    format: 'iife',
-    globalName: 'Exports',
-    bundle: true,
-    minify: true,
-    watch: process.argv.includes('--watch'),
-    loader: {".ts": "ts"}
-})
-.then(() => {
-const templateFile = fs.readFileSync('./engine/export_template.html', {encoding: 'utf-8'});
-const rawFile = fs.readFileSync('./_compiled/engine_raw.js', {encoding: 'utf-8'});
-const formatted =
-`export const EngineRawText = \`${
-    rawFile
-    .replace('var Exports', 'window.Exports')
-    .replace(/\\/g, "\\\\")
-    .replace(/\$/g, "\\$")
-    .replace(/'/g, "\\'")
-    .replace(/"/g, "\\\"")
-    .replace(/`/g, "\\\`")
-}\`;
+require("esbuild")
+    .build({
+        entryPoints: ['..\\engine\\Engine.ts'],
+        outfile: '.\\_compiled\\engine_raw.js',
+        format: 'iife',
+        globalName: 'Exports',
+        bundle: true,
+        minify: true,
+        watch: process.argv.includes('--watch'),
+        loader: {".ts": "ts"}
+    })
+    .then(() => {
+        const templateFile = fs.readFileSync('./engine/export_template.html', {encoding: 'utf-8'});
+        const rawFile = fs.readFileSync('./_compiled/engine_raw.js', {encoding: 'utf-8'});
+        const formatted =
+        `export const EngineRawText = \`${
+            rawFile
+            .replace('var Exports', 'window.Exports')
+            .replace(/\\/g, "\\\\")
+            .replace(/\$/g, "\\$")
+            .replace(/'/g, "\\'")
+            .replace(/"/g, "\\\"")
+            .replace(/`/g, "\\\`")
+            .replace(/\n/g, "")
+        }\`;
 
-export const HTMLTemplate = \`
-${templateFile}\`;
+        export const HTMLTemplate = \`
+        ${templateFile}\`;
 
-export const {Engine, Core} = (()=>{
-    eval?.(EngineRawText);
-    const {Engine, Core} = window.Exports;
-    delete window.Exports;
-    return {Engine, Core};
-})();
-`;
+        export const {Engine, Core} = (()=>{
+            eval?.(EngineRawText);
+            const {Engine, Core} = window.Exports;
+            delete window.Exports;
+            return {Engine, Core};
+        })();
+        `;
 
-if (!fs.existsSync(outputPath)){
-    fs.mkdirSync(outputPath);
-}
+        if (!fs.existsSync(outputPath)){
+            fs.mkdirSync(outputPath);
+        }
 
-fs.unlinkSync('./_compiled/engine_raw.js');
-fs.writeFileSync(outputPath + 'Engine.js', formatted);
-});
+        fs.unlinkSync('./_compiled/engine_raw.js');
+        fs.writeFileSync(outputPath + 'Engine.js', formatted);
+    });
